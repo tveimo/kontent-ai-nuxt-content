ee85574ae48f53f68219bda8fa4ebe7013816c9d
torgeir <torgeir@pobox.com>
2025-01-07 20:56:56 +1000

    simplified aside query using nuxt content helper

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -1,13 +1,11 @@
 <template>
   <aside aria-label="Aside">
-    <ul class="listbox" v-if="navigation2?.length > 0">
+    <ul class="listbox" v-if="inSection?.length > 0">
       <li class="heading">In this section</li>
-        <div v-for="dir in navigation2" :key="dir._path" >
-          <div v-for="link in dir.children" :key="link._path">
-            <li :class="{selected: link._path == page._path}" v-if="link._path != '/'"><a :href="link._path">
-              {{ link.title }}
-            </a></li>
-          </div>
+        <div v-for="link in inSection" :key="link._path" >
+          <li :class="{selected: link._path == page._path}" v-if="link._path != '/'"><a :href="link._path">
+            {{ link.title }}
+          </a></li>
         </div>
     </ul>
   </aside>
@@ -26,9 +24,16 @@ if (page.value.type == 'article' || page.value.type == 'product' && path.lastInd
   section = false;
   path = path.substring(0, path.lastIndexOf("/"));
 }
-let currentPathQuery = queryContent({where: {_path: { $contains: path }}})
-if (path == "/") {
-  currentPathQuery = queryContent()
+
+const { data: navigation } = await useAsyncData(path + '_navigation', () => fetchContentNavigation())
+
+const {
+  navDirFromPath,
+} = useContentHelpers()
+
+let inSection = navDirFromPath(path, navigation.value)
+if (!inSection) {
+  inSection = navigation;
 }
-const { data: navigation2 } = await useAsyncData(path + '_navigation', () => fetchContentNavigation(currentPathQuery))
+
 </script>
